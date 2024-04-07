372b1f450e06f6f26fae984a05ae7c028aeec270
Maruf Hossain <mdmarufh974@gmail.com>
2024-04-08 04:48:05 +0600

    find your new home section added

---
+++ src/Components/Slider/Slider.jsx
@@ -0,0 +1,87 @@
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Keyboard, Mousewheel, Navigation, Pagination, } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import img1 from '../../assets/images/025.jpg';
+import img3 from '../../assets/images/050.jpg';
+import img4 from '../../assets/images/103.jpg';
+import img5 from '../../assets/images/113.jpg';
+import img2 from './../../assets/images/028.jpg';
+
+const Slider = () => {
+    return (
+        <div className='px-[300px] bg-[#F3F3FA] py-10'>
+            <Swiper
+                cssMode={true}
+                navigation={true}
+                pagination={true}
+                mousewheel={true}
+                keyboard={true}
+                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="w-full md:h-[600px] text-white text-center">
+                        <img className='w-full h-[600px] absolute rounded-xl' src={img1} alt="" />
+                        <div className='relative top-[50%]'>
+                            <h2 className='text-5xl '>
+                                this is Slider 1.
+                            </h2>
+                            <button className='bg-red-500 p-5'>Book Now</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="w-full md:h-[600px] text-white text-center">
+                        <img className='w-full h-[600px] absolute rounded-xl' src={img2} alt="" />
+                        <div className='relative top-[50%]'>
+                            <h2 className='text-5xl '>
+                                this is Slider 2.
+                            </h2>
+                            <button className='bg-red-500 p-5'>Book Now</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="w-full md:h-[600px] text-white text-center">
+                        <img className='w-full h-[600px] absolute rounded-xl' src={img3} alt="" />
+                        <div className='relative top-[50%]'>
+                            <h2 className='text-5xl '>
+                                this is Slider 3.
+                            </h2>
+                            <button className='bg-red-500 p-5'>Book Now</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="w-full md:h-[600px] text-white text-center">
+                        <img className='w-full h-[600px] absolute rounded-xl' src={img4} alt="" />
+                        <div className='relative top-[50%]'>
+                            <h2 className='text-5xl '>
+                                this is Slider 4.
+                            </h2>
+                            <button className='bg-red-500 p-5'>Book Now</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="w-full md:h-[600px] text-white text-center">
+                        <img className='w-full h-[600px] absolute rounded-xl' src={img5} alt="" />
+                        <div className='relative top-[50%]'>
+                            <h2 className='text-5xl '>
+                                this is Slider 5.
+                            </h2>
+                            <button className='bg-red-500 p-5'>Book Now</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                
+
+            </Swiper>
+        </div>
+    );
+};
+
+export default Slider;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,41 @@
+import Slider from "../../Components/Slider/Slider";
+import icon1 from "../../assets/images/Icon (1).png";
+import icon2 from "../../assets/images/Icon.png";
+import icon3 from "../../assets/images/Iconicon (1).png";
+import house from "../../assets/images/one (3).jpg";
 const Home = () => {
     return (
         <div>
-            
+            <Slider></Slider>
+            <div className="bg-[#E0DEF7] border-2 border-[E0DEF7] lg:px-[300px] flex py-20">
+                <div className="flex flex-col justify-around">
+                    <div>
+                        <h2 className="text-6xl font-bold mb-10">The new way to find your new home</h2>
+                        <p>Find your dream place to live in with more than 10k+ properties listed.</p>
+                    </div>
+                    <div className="flex justify-around text-center">
+                        <div>
+                            <img className="drop-shadow-lg w-1/2 mx-auto text-center" src={icon3} alt="" />
+                            <p className="font-bold text-3xl my-3">7.4%</p>
+                            <p>Property Revinue Rate</p>
+                        </div>
+                        <div>
+                            <img className="drop-shadow-lg w-1/2 mx-auto text-center" src={icon2} alt="" />
+                            <p className="font-bold text-3xl my-3">7.4%</p>
+                            <p>Property Revinue Rate</p>
+                        </div>
+                        <div>
+                            <img className="drop-shadow-lg w-1/2 mx-auto text-center" src={icon1} alt="" />
+                            <p className="font-bold text-3xl my-3">7.4%</p>
+                            <p>Property Revinue Rate</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div>
+                    <img className="rounded" src={house} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
