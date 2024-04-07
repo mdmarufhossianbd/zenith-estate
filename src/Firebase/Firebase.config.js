8a6c5e0fedb9f2816915d63b3c66f1e69d66eca0
Maruf Hossain <mdmarufh974@gmail.com>
2024-04-07 23:14:22 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5Dj946atfEIcNTJkObMBn_CjosgajoXQ",
+  authDomain: "zenith-estate-19bb2.firebaseapp.com",
+  projectId: "zenith-estate-19bb2",
+  storageBucket: "zenith-estate-19bb2.appspot.com",
+  messagingSenderId: "1008085205491",
+  appId: "1:1008085205491:web:36c642df808643bb5f9286"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
