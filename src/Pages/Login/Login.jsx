1568bed5071f9ca911f060b5d7493e750386c7db
Maruf Hossain <mdmarufh974@gmail.com>
2024-04-08 23:03:56 +0600

    login & register page responsive done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,13 @@
 import { useContext } from "react";
+import { AiFillGoogleCircle } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider";
 
+
 const Login = () => {
 
-    const {loginUser} = useContext(AuthContext)
+    const { loginUser } = useContext(AuthContext)
 
     const handlelogin = e => {
         e.preventDefault();
@@ -12,30 +15,37 @@ const Login = () => {
         const password = e.target.password.value;
         console.log(email, password);
         loginUser(email, password)
-        .then(result=>{
-            result.user
-            console.log(result, "user create successfully")
-        })
-        .catch(error=>{
-            error.message
-            console.log(error.message);
-        })
+            .then(result => {
+                result.user
+                console.log(result, "user create successfully")
+            })
+            .catch(error => {
+                error.message
+                console.log(error.message);
+            })
     }
     return (
-        <div className=" h-screen bg-[#000000] bg-[url('https://i.ibb.co/bBwLgrt/real-state-banner-3.png')] bg-blend-screen bg-cover bg-no-repeat pt-[8%]">
-            <div className="lg:w-1/2 xl:w-1/3 md:mx-10 mx-3 bg-[#00000060] lg:mx-auto text-center p-10 rounded-md mix-blend-normal">
+        <div className="h-screen w-screen bg-[#000] bg-[url('https://i.ibb.co/D4p1Y1x/real-state-banner-2.png')] bg-blend-screen bg-cover bg-no-repeat lg:pt-[10%] md:pt-[20%] pt-[20%]">
+            <div className=" xl:w-1/3 lg:w-1/2 md:w-1/2 mx-5 bg-[#49494985] lg:mx-auto md:mx-auto text-center p-10 rounded-md">
                 <h2 className="text-4xl text-white font-bold">Please Login</h2>
-                <hr className="mx-[20%] my-5" />
+                <hr className="mx-[30%] my-5" />
                 <form onSubmit={handlelogin} >
-                    <div className="flex flex-col gap-4 m-8">
-                        
-                        <input className="bg-[#100A55] p-3 text-lg text-white placeholder:text-white rounded-md" type="email" name="email" placeholder="Your Email" />
-                        <input className="bg-[#100A55] p-3 text-lg text-white placeholder:text-white rounded-md" type="password" name="password" placeholder="Password" />                        
-                        <button type="submit" className="bg-[#100A55] p-3 text-lg font-medium text-white rounded-md hover:bg-purple-600  hover:text-white "  >Login</button>
-                        <p className="text-xl font-medium text-white">Create an account. <Link className="text-purple-700 hover:text-white hover:underline" to={'/register'}>Register</Link></p>
+                    <div className="flex flex-col gap-4 mt-8">
+
+                        <input className="bg-[#100a5500] border-b-2 p-3 text-lg text-white placeholder:text-white rounded-md" type="email" name="email" placeholder="Your Email" />
+                        <input className="bg-[#100a5500] border-b-2 p-3 text-lg text-white placeholder:text-white rounded-md" type="password" name="password" placeholder="Password" />
+
+                        <button type="submit" className="bg-[#100A55] p-3 text-lg font-medium text-white rounded-md hover:bg-purple-600  hover:text-white " >Login</button>
+                        <p className="text-xl font-medium text-white">You have not an account. <Link className="text-white underline hover:text-purple-600" to={'/register'}>Register</Link></p>
                     </div>
-                    {/* <input type="button" value="" /> */}
                 </form>
+                <div className="text-white flex flex-col items-center justify-center gap-4 p-4">
+                    <button className="flex gap-3 text-xl items-center bg-[#5245ED] p-2 rounded hover:bg-purple-600"><AiFillGoogleCircle /> Log in with Google 
+                    </button>
+                    <button className="flex gap-3 text-xl items-center bg-purple-600 p-2 rounded hover:bg-[#5245ED]"><FaGithub /> Log in with GitHub 
+                    </button>
+                    
+                </div>
             </div>
         </div>
     );
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -13,7 +13,6 @@ const Register = () => {
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(name, photo, email, password);
-
         createUser(email, password)
             .then(result => {
                 result.user
@@ -26,22 +25,22 @@ const Register = () => {
     }
 
     return (
-        <div className=" h-screen bg-[#000000] bg-[url('https://i.ibb.co/D4p1Y1x/real-state-banner-2.png')] bg-blend-screen bg-cover bg-no-repeat pt-[8%]">
+        <div className=" h-screen bg-[#000000] bg-[url('https://i.ibb.co/D4p1Y1x/real-state-banner-2.png')] bg-blend-screen bg-cover bg-no-repeat lg:pt-[10%] md:pt-[20%] pt-[20%]">
 
-            <div className="lg:w-1/3 md:mx-10 mx-3 bg-[#777777] lg:mx-auto text-center p-10 rounded-md mix-blend-screen">
+            <div className="xl:w-1/3 lg:w-1/2 md:w-1/2 mx-5 bg-[#49494985] lg:mx-auto md:mx-auto text-center p-10 rounded-md">
                 <h2 className="text-4xl text-white font-bold">Please Create an account</h2>
                 <hr className="mx-[20%] my-5" />
                 <form onSubmit={handleRegister} >
                     <div className="flex flex-col gap-4 m-8">
-                        <input className=" bg-[#100A55] p-3 text-lg text-white placeholder:text-white rounded-md" type="text" name="name" placeholder="Your Name" />
-                        <input className="bg-[#100A55] p-3 text-lg text-white placeholder:text-white rounded-md" type="text" name="photo" placeholder="Your Photo url" />
-                        <input className="bg-[#100A55] p-3 text-lg text-white placeholder:text-white rounded-md" type="email" name="email" placeholder="Your Email" />
-                        <input className="bg-[#100A55] p-3 text-lg text-white placeholder:text-white rounded-md" type="password" name="password" placeholder="Password" />
+                        <input className="bg-[#100a5500] border-b-2 p-3 text-lg text-white placeholder:text-white rounded-md" type="text" name="name" placeholder="Your Name" />
+                        <input className="bg-[#100a5500] border-b-2 p-3 text-lg text-white placeholder:text-white rounded-md" type="text" name="photo" placeholder="Your Photo url" />
+                        <input className="bg-[#100a5500] border-b-2 p-3 text-lg text-white placeholder:text-white rounded-md" type="email" name="email" placeholder="Your Email" />
+                        <input className="bg-[#100a5500] border-b-2 p-3 text-lg text-white placeholder:text-white rounded-md" type="password" name="password" placeholder="Password" />
                         
-                        <button type="submit" className="bg-[#100A55] p-3 text-lg font-medium text-white rounded-md hover:bg-purple-600  hover:text-white " >Register</button>
-                        <p className="text-xl font-medium text-white">Have have an account. <Link className="text-[#5245ED] hover:text-purple-600 hover:underline" to={'/login'}>Login</Link></p>
+                        <button type="submit" className="bg-[#5245ED] p-3 text-lg font-medium text-white rounded-md hover:bg-[#100A55]  hover:text-white " >Register</button>
+                        <p className="text-xl text-white">Have have an account. <Link className="text-white underline hover:text-purple-600" to={'/login'}>Login</Link></p>
                     </div>
-                    {/* <input type="button" value="" /> */}
+                    
                 </form>
             </div>
         </div>
